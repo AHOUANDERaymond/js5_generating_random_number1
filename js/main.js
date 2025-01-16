d9c6428df29b3173c1e3e53df65beababc22edae
AHOUANDERaymond <raymondahouande@gmail.com>
2025-01-15 18:36:57 -0800

    Generer des numeros aux hasard

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//Maths methods and properties
+
+
+
+console.log(Math.floor(Math.random()*10)+1);
+
+console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+console.log(Math.floor(Math.random()*10)+1);
+
+console.log(Math.floor(Math.random()*10)+1);
+
+
