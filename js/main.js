0c7ede6b246169c805a94b777ffcfddc6a645eea
AHOUANDERaymond <raymondahouande@gmail.com>
2025-01-15 19:24:47 -0800

    Mes premiers challenges

--- js/main.js
+++ js/main.js
@@ -1,14 +1,7 @@
-//Maths methods and properties
+//Your first code challenge
 
+//Write code that will return a random letter from my name
 
-
-console.log(Math.floor(Math.random()*10)+1);
-
-console.log(Math.floor(Math.random()*10)+1);
-console.log(Math.floor(Math.random()*10)+1);
-
-console.log(Math.floor(Math.random()*10)+1);
-
-console.log(Math.floor(Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10+15))
 
 
