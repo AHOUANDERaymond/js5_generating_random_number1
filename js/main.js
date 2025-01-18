4d3a58702a2cc82f6b8e0ef79900ae6709952de5
AHOUANDERaymond <raymondahouande@gmail.com>
2025-01-17 17:20:31 -0800

    Comment avoir des noms choisis a tout hasard

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 //Your first code challenge
 
-//Write code that will return a random letter from my name
+//Write code that will return a random name  from my list
 
-console.log(Math.ceil(Math.random()*10+67))
+//console.log("Raymond Ray".charAt(Math.floor(Math.random()*4+1)));
+
+
+const prenoms = ["Raymond", "Ray", "Jean", "Marie", "Pierre", "Claire"];
+const randomPrenom = prenoms[Math.floor(Math.random() * prenoms.length)];
+
+console.log(randomPrenom);
 
 
