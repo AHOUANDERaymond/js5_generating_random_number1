59086272528564b0085e1ff29af4f3a85aa82c47
AHOUANDERaymond <raymondahouande@gmail.com>
2025-01-17 18:40:02 -0800

    cCommande de soupe et crackers version corrigee

--- js/main.js
+++ js/main.js
@@ -1,26 +1,20 @@
-//Conditionals: If statements
-
-let customerIsBanned=true;
-let soup="chicken noodle soup"
+let customerIsBanned = true;
+let soup = "chicken noodle soup";
 let reply;
-let crackers=true;
+let crackers = true;
 
-if (customerIsBanned){
-    reply="No soup for you!";
-} else if (soup & crackers) {
-    reply='Here is your order of ${soup} & crackers';
+if (customerIsBanned) {
+    reply = "No soup for you!";
+} else if (soup && crackers) {  // Utilisation de `&&` pour vérifier les deux
+    reply = `Here is your order of ${soup} & crackers`;  // Utilisation de backticks pour les variables
 } else if (soup) {
-    reply='Here is your order of ${soup}';
+    reply = `Here is your order of ${soup}`;
 }
 
-
-
- 
-if (soup){
-    reply='Here is your order of $(soup)';
-    
-}else{
-    reply='Sorry, we are out of $(soup)';
-
+if (soup) {
+    reply = `Here is your order of ${soup}`;  // Correction avec backticks
+} else {
+    reply = `Sorry, we are out of ${soup}`;  // Correction avec backticks
 }
-console.log(reply)
+
+console.log(reply);
