07cd21db08faa06f9d4ce5d40c0bc9b3879b607e
AHOUANDERaymond <raymondahouande@gmail.com>
2025-01-18 23:43:50 -0800

    Exemple pratique

--- js/main.js
+++ js/main.js
@@ -1,24 +1,20 @@
 //Conditionals: If Statements
 
-let testScore=889;
-let grade;
-let collegeStudent=true;
+//Decisions trees!
+if (playerOne ===computer){
+    //tie game
 
+}else if (playerOne==="rock"){
+    if (computer==="paper"){
+        //Computer wins
 
-if (testScore>=90){
-    grade="A";
-} else if (testScore>=80){
-    grade="B";
-}else if (testScore>=70){
-    grade="C";
-}else if (testScore>=60){
-    grade="D";
-}else {
-    //Nested if statement
-    if (collegeStudent)
-    grade="U";
-}
+    }else{
+        //playerOne wins
+    }
+}else if (playerTwo==="paper"){
+    if (computer==="scissors")
+        //Computer wins
 
-
-
-console.log(grade)
+    }
+        //playerOne wins
+console.log(playerOne)
