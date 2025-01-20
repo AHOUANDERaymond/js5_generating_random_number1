a65c6ee8341b420b59d8804d570e78bd54b8a51e
AHOUANDERaymond <raymondahouande@gmail.com>
2025-01-19 23:51:57 -0800

    exemple a annuler

--- js/main.js
+++ js/main.js
@@ -17,4 +17,22 @@ if (playerOne ===computer){
 
     }
         //playerOne wins
+
+
+        if (playertwo ===computer){
+            //tie game
+        
+        }else if (playerOne==="rock"){
+            if (computer==="paper"){
+                //Computer wins
+        
+            }else{
+                //playerOne wins
+            }
+        }else if (playerTwo==="paper"){
+            if (computer==="scissors")
+                //Computer wins
+        
+            }
+                //playerOne wins
 console.log(playerOne)
