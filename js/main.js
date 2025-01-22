afc5bde2d988efcfd9e9d6c96d7edca99e6d38d1
AHOUANDERaymond <raymondahouande@gmail.com>
2025-01-22 12:29:49 -0800

    Suite des exercices sur les joueurs

--- js/main.js
+++ js/main.js
@@ -3,22 +3,23 @@
 
 
 
-switch(expression OR value){
-    case choice1:
-        console.log("No match")
+switch("2"){
+    case 1:
+        console.log("1")
         //run this code
         break;
-    case choice2:
-        console.log("No match")
+    case 2:
+        console.log("2")
         //run this different  code
     break;
 
-    case choice2:
-        console.log("No match")
+    case 2:
+        console.log("1")
         //run this different  code
-    break;
+        break;
     //add as many code as needed
     default:
+        console.log("3")
         //run   this code if no case matches
         //no need for a break here
 }
