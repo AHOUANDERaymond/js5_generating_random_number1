72e126d9bcd79ee08d93d127d5cc6be277c1ea40
AHOUANDERaymond <raymondahouande@gmail.com>
2025-01-23 23:44:11 -0800

    Exercice sur le ternary operator nbre d'annees d'experience

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 // //Conditionals: Ternary Operator
 
-// Définition des variables
-let age = 45;
-let revenusMensuels = 3000;
 
-// Utilisation de l'opérateur ternaire pour vérifier l'éligibilité
-let eligibilite = 
-    age >= 18 && revenusMensuels >= 5000
-        ? "Eligible pour un prêt"
-        : "Non éligible pour un prêt";
+// Définition des variables
+let experience = 5;  // nombre d'années d'expérience
+let evenementDifficulte = "avancé";  // difficulté de l'événement, mais on s'en fiche maintenant
+
+// Utilisation de l'opérateur ternaire sans condition sur l'événement
+let participation = 
+    experience >= 5
+        ? "Peut participer à l'événement"
+        : "Ne peut pas participer à l'événement";
 
 // Affichage du résultat
-console.log(`La personne est : ${eligibilite}`);
+console.log(participation);
+
+
