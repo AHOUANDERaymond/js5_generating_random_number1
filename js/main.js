d0014f33657099a245754588cd7d2dc60d26144f
AHOUANDERaymond <raymondahouande@gmail.com>
2025-01-24 19:06:10 -0800

    Exo 1 sur le USER INPUT

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
 // User input
 
+let email = prompt("Please, enter your email address.");
 
+if (email) {
+  // Vérification de la validité de l'email
+  if (email.includes("@")) {
+    console.log("Adresse valide");
+  } else {
+    console.log("Adresse invalide");
+  }
 
-// let myBoolean=confirm("Ok===True\nCancel===False");
-// console.log(myBoolean);
-
-
-let name= prompt("Please, enter your name.");
-if (name){
-  console.log(name.length);
-  console.log(name.trim().length);
-  console.log(name.trim());
-
-}else{
-    console.log("You didn't enter your name");
+  // Affichage de la longueur avant et après trim
+  console.log("Longueur avant trim : " + email.length);
+  console.log("Longueur après trim : " + email.trim().length);
+  console.log("Email après trim : " + email.trim());
+} else {
+  console.log("Vous n'avez pas entré d'adresse email.");
 }
