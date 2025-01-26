0198559422badfcdffabfa98628a7e1454136c87
AHOUANDERaymond <raymondahouande@gmail.com>
2025-01-25 23:20:36 -0800

    Premier jeu interactif

--- js/main.js
+++ js/main.js
@@ -1,19 +1,25 @@
-// User input
+// Your First Interactive Game
+let playGame= confirm("Shall we play rock, paper, or scissors?");
+if (playGame){
+    //play
+    let playerChoice =promt("Please, enter rock, paper or scissors");
 
-let email = prompt("Please, enter your email address.");
+    if(playerChoice) {
+        let playerOne= playerChoice.trim().toLowerCase();
+        if (playerOne==="rock"|| playerOne==="paper" ||playerOne==="scissors"){
 
-if (email) {
-  // Vérification de la validité de l'email
-  if (email.includes("@")) {
-    console.log("Adresse valide");
-  } else {
-    console.log("Adresse invalide");
-  }
 
-  // Affichage de la longueur avant et après trim
-  console.log("Longueur avant trim : " + email.length);
-  console.log("Longueur après trim : " + email.trim().length);
-  console.log("Email après trim : " + email.trim());
-} else {
-  console.log("Vous n'avez pas entré d'adresse email.");
+            let computerChoice=Math.foor(Math.random()*3+1);
+            let computer= computerChoice===1? "rock"
+                :computerChoice===2? "paper"
+                :"scissors"
+        }else{
+            alert("You didn't enter rock,paper or scissors")
+        }
+
+    }else{
+        alert("I guess you changed your mind. Maybe next time.")
+    }
+}else{
+ alert("Ok,maybe next time.");
 }
