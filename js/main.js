bf15d26742a57c75f510d93f1c62e8cdff5dc9b4
AHOUANDERaymond <raymondahouande@gmail.com>
2025-01-27 18:16:19 -0800

    Revision du jeu

--- js/main.js
+++ js/main.js
@@ -10,26 +10,26 @@ if (playGame){
         if (playerOne==="rock"|| playerOne==="paper" ||playerOne==="scissors"){
 
 
-            let computerChoice=Math.foor(Math.random()*3+1);
-            let computer= computerChoice===1? "rock"
-                :computerChoice===2? "paper"
-                :"scissors"
-            let playerOne ="paper";
-            let computer ="rock";
-            let result = 
-                playerOne===computer? 
-                    "Tie game!"
-                    : playerOne==="rock" && computer ==="paper" ? "computer wins" ? playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!
+        let computerChoice=Math.foor(Math.random()*3+1);
+        let computer= computerChoice===1? "rock"
+            :computerChoice===2? "paper"
+            :"scissors"
+        let playerOne ="paper";
+        let computer ="rock";
+        let result = 
+            playerOne===computer? 
+                "Tie game!"
+                : playerOne==="rock" && computer ==="paper" ? "computer wins" ? playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!
 
-                    : playerOne==="paper" && computer ==="scissors" ? "computer wins" ? "computer wins" ? playerOne: ${playerOne}\nComputer: ${computer}\ncomputer wins!
-                    : playerOne==="scissors" && computer ==="rock" ? "computer wins" ? ? "computer wins" ? playerOne: ${playerOne}\nComputer: ${computer}\ncomputer wins!
-                    ? "computer wins" ? playerOne: ${playerOne}\nComputer: ${computer}\nplayerOne wins!
+                : playerOne==="paper" && computer ==="scissors" ? "computer wins" ? "computer wins" ? playerOne: ${playerOne}\nComputer: ${computer}\ncomputer wins!
+                : playerOne==="scissors" && computer ==="rock" ? "computer wins" ? ? "computer wins" ? playerOne: ${playerOne}\nComputer: ${computer}\ncomputer wins!
+                ? "computer wins" ? playerOne: ${playerOne}\nComputer: ${computer}\nplayerOne wins!
         }else{
             alert("You didn't enter rock,paper or scissors")
         }
 
     }else{
-        alert("I guess you changed your mind. Maybe next time.")
+        alert("I guess you changed your mind. Maybe another time.")
     }
 }else{
  alert("Ok,maybe next time.");
