4df3504e33837fc333780d918eaf73df42f04673
AHOUANDERaymond <raymondahouande@gmail.com>
2025-01-27 18:57:13 -0800

    do while loop

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 // Loops
-let myNumber = 0;
-while (myNumber<=50){
+let myNumber = 50;
+while (myNumber < 50) {
     myNumber += 2;
     console.log(myNumber);
 }
 
 //Don't create an endless loop
+
+do {
+    console.log(myNumber)
+}while (myNumber < 50);
