a48449fa9502947a98b001e9e5c935713d68888e
AHOUANDERaymond <raymondahouande@gmail.com>
2025-01-27 18:46:11 -0800

    While loop

--- js/main.js
+++ js/main.js
@@ -1,36 +1,8 @@
-// Your First Interactive Game
-
-let playGame= confirm("Shall we play rock, paper, or scissors?");
-if (playGame){
-    //play
-    let playerChoice =promt("Please, enter rock, paper or scissors");
-
-    if(playerChoice) {
-        let playerOne= playerChoice.trim().toLowerCase();
-        if (playerOne==="rock"|| playerOne==="paper" ||playerOne==="scissors"){
-
-
-        let computerChoice=Math.foor(Math.random()*3+1);
-        let computer= computerChoice===1? "rock"
-            :computerChoice===2? "paper"
-            :"scissors"
-        let playerOne ="paper";
-        let computer ="rock";
-        let result = 
-            playerOne===computer? 
-                "Tie game!"
-                : playerOne==="rock" && computer ==="paper" ? "computer wins" ? playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!
-
-                : playerOne==="paper" && computer ==="scissors" ? "computer wins" ? "computer wins" ? playerOne: ${playerOne}\nComputer: ${computer}\ncomputer wins!
-                : playerOne==="scissors" && computer ==="rock" ? "computer wins" ? ? "computer wins" ? playerOne: ${playerOne}\nComputer: ${computer}\ncomputer wins!
-                ? "computer wins" ? playerOne: ${playerOne}\nComputer: ${computer}\nplayerOne wins!
-        }else{
-            alert("You didn't enter rock,paper or scissors")
-        }
-
-    }else{
-        alert("I guess you changed your mind. Maybe another time.")
-    }
-}else{
- alert("Ok,maybe next time.");
+// Loops
+let myNumber = 0;
+while (myNumber<=50){
+    myNumber += 2;
+    console.log(myNumber);
 }
+
+//Don't create an endless loop
