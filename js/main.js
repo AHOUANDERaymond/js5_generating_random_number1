32c8a9c3317c1ef26eb8fa8238f37c2139ce10d8
AHOUANDERaymond <raymondahouande@gmail.com>
2025-01-31 18:48:40 -0800

    La fonction: cas de l'exercice sur la somme

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 //Functions
 
-//Methods = Built-in functions
-"Raymond".toLowerCase();
-Math.random();
+//Functions declaration Syntax:
+function sum(numb1, numb2) {
+  if (numb2 === undefined) {
+    return numb1 + numb1;
+    
+  }
+
+  return numb1+numb2;
+
+}
+console.log(sum(2,6));
 
 
