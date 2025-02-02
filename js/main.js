541731a2c7ff1bd8c1de73eac1d4326ab13a5aef
AHOUANDERaymond <raymondahouande@gmail.com>
2025-02-01 18:33:45 -0800

    Arrays: utilisation et astuces

--- js/main.js
+++ js/main.js
@@ -1,47 +1,11 @@
-// var, let and const
+//Arrays
 
+const myArray = [];
 
-// Variables globales
-var globalVar = "Je suis var";
-let globalLet = "Je suis let";
-const globalConst = "Je suis const";
+//add elements to an array
+myArray[0] = "Raymond";
+myArray[1] = 1011;
+myArray[2] = false;
 
-console.log("Avant la fonction:");
-console.log("Global var:", globalVar);
-console.log("Global let:", globalLet);
-console.log("Global const:", globalConst);
-
-function testScope() {
-    var globalVar = "Var dans la fonction";
-    let globalLet = "Let dans la fonction";
-    // globalConst = "Const dans la fonction"; 
-    // Que se passe-t-il si on essaie de modifier globalConst ?
-
-    console.log("\nDans la fonction:");
-    console.log("Var:", globalVar);
-    console.log("Let:", globalLet);
-    console.log("Const:", globalConst);
-
-    {
-        var globalVar = "Var dans le bloc";
-        let globalLet = "Let dans le bloc";
-        const globalConst = "Const dans le bloc";
-
-        console.log("\nDans le bloc:");
-        console.log("Var:", globalVar);
-        console.log("Let:", globalLet);
-        console.log("Const:", globalConst);
-    }
-
-    console.log("\nAprès le bloc, dans la fonction:");
-    console.log("Var:", globalVar); // Que remarquez-vous ici ?
-    console.log("Let:", globalLet);
-    console.log("Const:", globalConst);
-}
-
-testScope();
-
-console.log("\nAprès la fonction:");
-console.log("Global var:", globalVar); // Est-ce que la valeur a changé ?
-console.log("Global let:", globalLet);
-console.log("Global const:", globalConst);
+//refer to an array
+console.log(myArray);
