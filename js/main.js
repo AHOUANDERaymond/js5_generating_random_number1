a39b46399962974d74fc79e03d1169b46931f065
AHOUANDERaymond <raymondahouande@gmail.com>
2025-02-03 23:39:57 -0800

    Ajout de la ligne "C"

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ const myArrayA = ["A", "B", "C", "C1"];
 
 const myArrayB = [ "D", "E", "F", "G"];
 
-const newArray = myArrayB.concat(myArrayA);
+const myArrayC = [ "D1", "E1", "F1", "G1"];
+
+const newArray = myArrayB.concat(myArrayA).concat(myArrayC);
 
 console.log(newArray);
