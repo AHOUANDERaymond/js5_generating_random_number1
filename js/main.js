dbbec2a69daa6412597e6de895743e29a3613174
AHOUANDERaymond <raymondahouande@gmail.com>
2025-02-04 22:29:11 -0800

    Exo

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ const myArrayA = ["A", "B", "C", "C1"];
 
 const myArrayB = [ "D", "E", "F", "G"];
 
-const myArrayC = [ "D1", "E1", "F1", "G1"];
+const myArrayB1 = [ "D1", "E1", "F1", "G1"];
 
-const newArray = myArrayB.concat(myArrayA).concat(myArrayC);
+const newArray = myArrayB.concat(myArrayA).concat(myArrayB1);
 
 console.log(newArray);
