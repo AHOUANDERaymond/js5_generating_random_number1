436e27c9b1f644a61d6a22d3bc7b548ff8c72990
AHOUANDERaymond <raymondahouande@gmail.com>
2025-02-05 18:10:04 -0800

    Autre facon de faire "concactenation

--- js/main.js
+++ js/main.js
@@ -4,8 +4,7 @@ const myArrayA = ["A", "B", "C", "C1"];
 
 const myArrayB = [ "D", "E", "F", "G"];
 
-const myArrayB1 = [ "D1", "E1", "F1", "G1"];
 
-const newArray = myArrayB.concat(myArrayA).concat(myArrayB1);
+const newArray = [myArrayA, myArrayB];
 
 console.log(newArray);
