ebdbe47345d59be6ecdfca854650aebb13e0f3cd
AHOUANDERaymond <raymondahouande@gmail.com>
2025-02-06 21:46:09 -0800

    Suite de Equipshelf

--- js/main.js
+++ js/main.js
@@ -15,5 +15,5 @@ console.log(clothesShelfB[0]);
 const equipDept = [equipShelfA, equipShelfB];
 const clothesDept = [clothesShelfA, clothesShelfB];
 
-console.log(equipDept);
-console.log(clothesDept)
+console.log(equipDept[0][1]);
+console.log(clothesDept[1][0]);
