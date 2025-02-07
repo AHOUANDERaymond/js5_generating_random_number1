28e9728f268f27215c909bb4d24ddb3b863efb02
AHOUANDERaymond <raymondahouande@gmail.com>
2025-02-06 23:16:50 -0800

    exo d'application

--- js/main.js
+++ js/main.js
@@ -1,23 +1,20 @@
-//Arrays
+//  Magasin de musique : Organisation des rayons
 
-const equipShelfA = ["baseball", "football", "volleyball"];
+//  Instruments
+const stringInstruments = ["guitare", "violon", "harpe"];
+const windInstruments = ["flûte", "saxophone", "trompette"];
 
-const equipShelfB = ["basketball", "golf balls", "tennis balls"];
+//  Accessoires
+const headphones = ["casque audio", "écouteurs", "micro"];
+const cables = ["câble jack", "câble USB", "câble XLR"];
 
-const clothesShelfA = ["tank tops", "t-shirts", "jerseys"];
+//  Regroupement en départements
+const instrumentsDept = [stringInstruments, windInstruments];
+const accessoriesDept = [headphones, cables];
 
-const clothesShelfB = ["sweat tops", "sweat pants", "hoodles"];
+//  Magasin entier
+const musicStore = [instrumentsDept, accessoriesDept];
 
-console.log(equipShelfA[1]);
-//or
-console.log(clothesShelfB[0]);
-
-const equipDept = [equipShelfA, equipShelfB];
-const clothesDept = [clothesShelfA, clothesShelfB];
-
-console.log(equipDept[0][1]);
-console.log(clothesDept[1][0]);
-
-const sportsStore = [equipDept, clothesDept];
-console.log(sportsStore[0][0][1]);
-console.log(sportsStore[1][1][0]);
+// Accès aux éléments
+console.log(musicStore[0][0][2]); // Affiche "harpe"
+console.log(musicStore[1][1][0]); // Affiche "câble jack"
