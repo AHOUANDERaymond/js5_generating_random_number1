afc4928dc371660f206f082beedf7f473bb67607
AHOUANDERaymond <raymondahouande@gmail.com>
2025-02-07 23:18:02 -0800

    Ajout de mon nom et prenom

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 //Objects
  //Key-value pairs in curly braces
- const myObj = { name: "Raymond"};
+ const myObj = { name: "AHOUANDE"};
+ const myObj1 = { firstname: "Raymond"};
+
 
  console.log(myObj.name);
+ console.log(myObj1.firstname);
