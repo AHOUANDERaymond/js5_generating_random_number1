a6528cce9cd9b5ae0dd570a954bd2463bc996c04
AHOUANDERaymond <raymondahouande@gmail.com>
2025-02-07 23:21:00 -0800

    Ajout de mail

--- js/main.js
+++ js/main.js
@@ -2,7 +2,10 @@
  //Key-value pairs in curly braces
  const myObj = { name: "AHOUANDE"};
  const myObj1 = { firstname: "Raymond"};
+ const myObj2 = { email: "raymondahouande@gmail.com"};
+
 
 
  console.log(myObj.name);
  console.log(myObj1.firstname);
+ console.log(myObj2.email);
