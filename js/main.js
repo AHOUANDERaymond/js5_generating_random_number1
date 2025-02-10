d1461a6f96d8f4c2ff2386378283e26cf3d1daa2
AHOUANDERaymond <raymondahouande@gmail.com>
2025-02-09 20:37:34 -0800

    Meme chose

--- js/main.js
+++ js/main.js
@@ -7,12 +7,9 @@
 
     }
  };
-  
- const truck = Object.create(vehicle);
- truck.doors = 2;
- console.log(truck);
- console.log(truck.wheels); //inheritance
- console.log(truck.engine());
+
+
+ 
  const car = Object.create(vehicle);
  car.doors = 4;
  car.engine = function(){
