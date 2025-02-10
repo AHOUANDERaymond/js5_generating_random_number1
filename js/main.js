5a027928ddcafb395e88f5f2b47dd85024cd83ff
AHOUANDERaymond <raymondahouande@gmail.com>
2025-02-09 20:03:01 -0800

    Notion de Objects

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 //Objects
  //Key-value pairs in curly braces
- const myObj = { name: "AHOUANDE"};
- const myObj1 = { firstname: "Raymond"};
- const myObj2 = { email: "raymondahouande@gmail.com"};
+ const myObj = { name: "Raymond"};
+
+ const infoObj = {
+    alive: true,
+    age: 30,
+    hobbies:["Eat", "Sleep", "Code"],
+    beverage: {
+        morning: "Coffee",
+        afternoon: "Tea"
+    }
 
 
 
- console.log(myObj.name);
- console.log(myObj1.firstname);
- console.log(myObj2.email);
+
+ }
+
+
+
+ console.log(infoObj["beverage"]);
+
