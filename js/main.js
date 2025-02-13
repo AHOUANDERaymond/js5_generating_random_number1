651dd7b1f6818615c468413125803412a4cc7682
AHOUANDERaymond <raymondahouande@gmail.com>
2025-02-12 18:49:25 -0800

    SEND and RECEIVE JSON

--- js/main.js
+++ js/main.js
@@ -18,3 +18,10 @@ const myObj = {
 console.log(myObj);
 console.log(myObj.name);
 myObj.hello();
+console.log(typeof myObj);
+
+const sendJSON = JSON.stringify(myObj);
+
+console.log(sendJSON);
+
+console.log(typeof sendJSON);
