b5604423b04ce9e66a080ab253751f72e148a1d4
AHOUANDERaymond <raymondahouande@gmail.com>
2025-02-14 15:19:01 -0800

    Gestion des erreurs

--- js/main.js
+++ js/main.js
@@ -5,10 +5,12 @@ const makeError = () => {
    let i=1;
    while (i<=5) {
       try{
+
          throw new Error("this is an error");
 
       }  catch (err) {
          console.error(err.name);
+         
          console.error(err.messages);
          console.error(err.stack);
 
